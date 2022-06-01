09417f444e60e645c76c8cbc82cae929cc522cce
Arnaldo Alamos <arnaldo.desarrollador.soft@gmail.com>
2022-06-01 13:30:46 -0400

    Segundo Commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
+var url        = window.location.href;
+var swLocation = '/twittor' 
+
+
 if( navigator.serviceWorker ){
-    console.log("entra")
-    navigator.serviceWorker.register('/sw.js')
+    if( url.includes('localhost')  ) {
+        swLocation = '/sw.js'
+    }
+    navigator.serviceWorker.register(swLocation)
 }
 
 
